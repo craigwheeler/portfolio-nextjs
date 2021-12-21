33c41562fd327cdbeab5705293e075ef86561e3f
Craig Wheeler <craig@craigwheeler.net>
2021-12-21 11:55:55 -0500

    create footer

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,13 @@
 import "tailwindcss/tailwind.css";
 import Navigation from "../components/Navigation";
+import Footer from "../components/Footer";
 
 const MyApp = ({ Component, pageProps }) => {
   return (
     <>
       <Navigation />
       <Component {...pageProps} />
+      <Footer />
     </>
   );
 };
